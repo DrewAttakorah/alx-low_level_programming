11984ff71586bb4577fa0017e182390bb917f403
DrewAttakorah <andyaw306@gmail.com>
2023-03-16 17:12:01 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - The main block is the start of the whole program
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
