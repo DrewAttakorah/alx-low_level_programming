bb7b20fd735d16bcf06348a31477a8fbcf5bdffe
DrewAttakorah <andyaw306@gmail.com>
2023-03-17 20:29:02 -0700

    Alpha Hour

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - this is the main func
+ *Description: Prints all the numbers of base 16 in lowercase
+ followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10){
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f'){
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		return (0);
+}
