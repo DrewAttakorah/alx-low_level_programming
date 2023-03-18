f5b619cc71d88ba2b349ef9cc2eafbf7ac5cd0fb
DrewAttakorah <andyaw306@gmail.com>
2023-03-17 20:07:07 -0700

    Alpha Hour

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-main code block in c
+ * Description: Prints alphabets from the back
+ * Return: 0
+ */
+int main(void)
+{
+	char ant = 'z';
+
+	for (ant = 0; ant <= 0; ant--)
+	{
+		putchar(ant);
+	}
+	putchar('\n');
+
+	return (0);
