9016cf62453f3119fd9c12eb7c8d0f18412977d6
DrewAttakorah <andyaw306@gmail.com>
2023-03-22 15:58:55 -0700

    10x Alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10x in lowercase
+ * REturn: 0
+ */
+void print_alphabets_10x(void)
+{
+	int i = 0;
+	char s;
+
+	while (i < 10)
+	{
+		s = 'a';
+
+		while (s <= 'z')
+		{
+			_putchar(s);
+			s++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
