3c2df5ceb2b538593fd5bc32344f39985d36aa0e
DrewAttakorah <andyaw306@gmail.com>
2023-03-22 10:24:59 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main code block
+ * Description: Print putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for Function Nested Loop Assignment
