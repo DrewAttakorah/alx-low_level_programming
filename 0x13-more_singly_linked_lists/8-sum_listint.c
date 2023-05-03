d1a8674394f1930f5f5769c6e331a067ad6b6259
DrewAttakorah <andyaw306@gmail.com>
2023-05-03 00:39:20 +0000

    sum update
    sum update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,8 +2,8 @@
 
 /**
  * sum_listint - get the sum of all the int `n` of a `listint_t` linked list
- * @head: a pointer to head
- * Return: a sum of all int `n`
+ * @head: pointer to head
+ * Return: sum of all int `n`
  * drewattakorah
  */
 int sum_listint(listint_t *head)
@@ -14,5 +14,5 @@ int sum_listint(listint_t *head)
 	for (my_sum = 0, current = head; current != NULL; current = current->next)
 		my_sum += current->n;
 
-	return (sum);
+	return (my_sum);
 }
